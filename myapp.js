a6c3547c473d6423764a19f0999c86aa2aace222
horus <carloch1234@gmail.com>
2024-03-10 17:25:49 -0500

    completado

--- myapp.js
+++ myapp.js
@@ -2,15 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const fs = require('fs')
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
+app.use(express.static(__dirname))
 
-app.use(express.static(__dirname + '/views'))
+//app.use(express.bodyParser());
+//app.post('/submit', function(req, res, next){
+    // req.body object has your form values
+   // console.log(req.body.nombre);
+ //});
 
+//man=?
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json())
+
+app.post("/submit", (req, res) => {
+    let datosejs = {nombre: req.body.nombre}
+    
+ return res.render("submit", {nombre : datosejs.nombre})
+
+})
+//servidor ok
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   });
 
+//rutas
 app.get('/', (req, res) => {
   res.render('home', {root : __dirname}) //con este too + '/views'
 });
@@ -27,7 +46,7 @@ app.get('/contact', (req, res) => {
     res.render('envio', { root: __dirname })
   });
 
-
+/*
   app.get('/submit', (req, res) => {
     res.render('submit', { root: __dirname })
-  });
+  });*/
